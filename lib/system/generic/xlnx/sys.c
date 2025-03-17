9c31a23c0c4e0c9066674eeda0ba7a60ce2171c6
Takeshi Yusa <takeshi-yusa@tessera.co.jp>
2025-03-17 15:28:56 +0900

    TEST COMMIT 002

--- lib/system/generic/xlnx/sys.c
+++ lib/system/generic/xlnx/sys.c
@@ -126,11 +126,8 @@ void *metal_machine_io_mem_map(void *va, metal_phys_addr_t pa,
 {
 	void *__attribute__((unused)) physaddr;
 
-#ifdef VERSAL_NET
-	va = metal_machine_io_mem_map_versal_net(va, pa, size, flags);
-#else
     va = Xil_MemMap(pa, size, flags);
 	//metal_assert(physaddr == (void *)pa);
-#endif
+
 	return va;
 }
