a9f3d4641f9a88ed2d08cdf74673f97b739b861c
Takeshi Yusa <takeshi-yusa@tessera.co.jp>
2025-03-17 18:28:36 +0900

    TEST COMMIT 007

--- lib/system/freertos/template/sys.c
+++ lib/system/freertos/template/sys.c
@@ -67,7 +67,7 @@ void *metal_machine_io_mem_map(void *va, metal_phys_addr_t pa,
 	/* Add implementation here */
 	void *__attribute__((unused)) physaddr;
 
-	va = Xil_MemMap(pa, size, flags);
+	physaddr = Xil_MemMap(pa, size, flags);
 
 	return va;
 }
